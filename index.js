4c60db132158d80e3351f2b68ee45d56e15acdb8
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-03-20 23:20:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`); // Step 3: Add message to array
+  }
+
+  return messages;
+}
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
